adb4aefb70bf580fe27f7047f32553009a1057e2
Alejandro Duarte <alejandro@vaadin.com>
2018-12-13 12:43:57 +0200

    Change scope of AuthenticationService bean to singleton

--- api/src/main/java/com/example/api/service/AuthenticationService.java
+++ api/src/main/java/com/example/api/service/AuthenticationService.java
@@ -4,6 +4,7 @@ import com.example.api.BusinessAppModule;
 import com.example.api.domain.User;
 import com.example.api.domain.UserRepository;
 import com.vaadin.flow.server.VaadinSession;
+import org.springframework.context.ApplicationContext;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -11,45 +12,39 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.annotation.SessionScope;
 
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.Map;
 
 @Component
-@SessionScope
 public class AuthenticationService {
 
     public static final String USER_ID_SESSION_KEY = "user_id";
 
     private final UserRepository userRepository;
-    private final Optional<BusinessAppModule[]> modules;
     private final PasswordEncoder passwordEncoder;
-
-    private boolean modulesInitialized;
+    private final ApplicationContext applicationContext;
 
     public AuthenticationService(UserRepository userRepository,
-                                 Optional<BusinessAppModule[]> modules,
-                                 PasswordEncoder passwordEncoder) {
+                                 PasswordEncoder passwordEncoder,
+                                 ApplicationContext applicationContext) {
         this.userRepository = userRepository;
-        this.modules = modules;
         this.passwordEncoder = passwordEncoder;
+        this.applicationContext = applicationContext;
     }
 
     public boolean authenticate(String username, String password) {
         User user = userRepository.findByEmailIgnoreCase(username);
 
         if (user != null && passwordEncoder.matches(password, user.getPassword())) {
-            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getId(),
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    user.getId(),
                     user.getPassword(),
-                    AuthorityUtils.createAuthorityList("ROLE_" + user.getRole().toString()));
+                    AuthorityUtils.createAuthorityList("ROLE_" + user.getRole().toString())
+            );
             SecurityContextHolder.getContext().setAuthentication(token);
             VaadinSession.getCurrent().setAttribute(USER_ID_SESSION_KEY, user.getId());
 
-            if (!modulesInitialized && modules.isPresent()) {
-                Arrays.stream(modules.get()).forEach(BusinessAppModule::initialize);
-                modulesInitialized = true;
-            }
+            initModules();
             return true;
         }
 
@@ -60,6 +55,11 @@ public class AuthenticationService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication.isAuthenticated() &&
                 !AnonymousAuthenticationToken.class.isAssignableFrom(authentication.getClass());
+    }
+
+    private void initModules() {
+        Map<String, BusinessAppModule> modules = applicationContext.getBeansOfType(BusinessAppModule.class);
+        modules.entrySet().stream().map(entry -> entry.getValue()).forEach(BusinessAppModule::initialize);
     }
 
 }
